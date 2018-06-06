92f260809e10920db06ca7fe53244dcd83237d15
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-06-06 23:18:40 +0300

    last psuh

---
+++ static/js/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./Search"
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> Hello React work1!</p>
+        <Search />
+        <p> Hello React work3!</p>
+      </div>
+    );
+  }
+}
